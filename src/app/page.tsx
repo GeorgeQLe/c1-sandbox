2467baf3f3f746a9382336f2017b435493f4ad4b
George Le <georgele@HB-M.local>
2025-02-21 11:49:19 -0500

    Fixed a visual bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <main className="flex flex-1 flex-col h-[calc(100vh-64px)] items-center justify-center">
-        <OnboardingFlow />
+      <main className="flex flex-col items-center justify-center pt-2">
+        <OnboardingFlow/>
       </main>
     </div>
   );
